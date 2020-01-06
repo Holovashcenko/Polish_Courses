650097f35998bf64851efb0c0312e0f887f10523
Vlad <holovashcenko@gmail.com>
2020-01-06 16:04:39 +0200

    added contact link

--- dist/es5/all.js
+++ dist/es5/all.js
@@ -1,22 +1,20 @@
 "use strict";
 
 $(document).ready(function () {
-  var link_main_one = $('#link_main_one');
-  var link_main_two = $('#link_main_two');
   var section_one = '<section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section><section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section>';
   var section_two = '<section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок Описание</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section>';
+  var section_three = '<section class="contact"><h3>Внимание</h3><p>Оставьте ваши контактные данные <br/>и наш консультант свяжется с вами</p><form id="application" action="application.php" method="POST" name="application "><input name="name" id="applicationName" maxlength="20" placeholder="Введите ваше имя" required /><input name="email" type="email" id="applicationEmail" maxlength="30" placeholder="Введите ваш E-mail" required/><input name="telephone" type="Tel" id="applicationTelephone" maxlength="20" placeholder="Введите ваш телефон" required /><button class="applicationButton" type="submit" form="application"> Оставить заявку </button></form></section>';
   $('#link_main_one').click(function () {
-    console.log('hello 1');
     $('section').detach();
     $('main').append(section_one);
   });
   $('#link_main_two').click(function () {
-    console.log('hello 2');
     $('section').detach();
     $('main').append(section_two);
   });
   $('#link_main_three').click(function () {
-    console.log('hello 3');
+    $('section').detach();
+    $('main').append(section_three);
   });
 });
 //# sourceMappingURL=all.js.map
