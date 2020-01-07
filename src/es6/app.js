e25dc45a96ecd8b0fede00e91b49f83572301bb1
Vlad <holovashcenko@gmail.com>
2020-01-07 21:16:34 +0200

    changed the app.js

--- src/es6/app.js
+++ src/es6/app.js
@@ -1,9 +1,41 @@
 
 $( document ).ready(function() {
 
-    var section_one = '<section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section><section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section>';
-    var section_two = '<section><article><div id="article_image"><img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt=""></div><div id="article_info"><h2>Заголовок Описание</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p></div></article></section>';
-    var section_three = '<section class="contact"><h3>Внимание</h3><p>Оставьте ваши контактные данные <br/>и наш консультант свяжется с вами</p><form id="application" action="application.php" method="POST" name="application "><input name="name" id="applicationName" maxlength="20" placeholder="Введите ваше имя" required /><input name="email" type="email" id="applicationEmail" maxlength="30" placeholder="Введите ваш E-mail" required/><input name="telephone" type="Tel" id="applicationTelephone" maxlength="20" placeholder="Введите ваш телефон" required /><button class="applicationButton" type="submit" form="application"> Оставить заявку </button></form></section>';
+    let section_one = `<section>
+                        <article>
+                            <div id="article_image">
+                                <img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt="">
+                            </div>
+                            <div id="article_info">
+                                <h2>Заголовок</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p>
+                            </div>
+                        </article>
+                      </section>
+                      <section>
+                        <article>
+                            <div id="article_image">
+                                <img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt="">
+                            </div>
+                            <div id="article_info">
+                                <h2>Заголовок</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p>
+                            </div>
+                        </article>
+                      </section>`;
+                      
+    let section_two = `<section>
+                        <article>
+                            <div id="article_image">
+                                <img src="./images/dose-media-bU6JyhSI6zo-unsplash.jpg" alt="">
+                            </div>
+                            <div id="article_info">
+                                <h2>Заголовок</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quos nostrum natusrepudiandae fuga inventore ullam culpa recusandae suscipit repellendus vero, asperiores vel assumenda ipsum! Corrupti, excepturi. Accusantium, officia omnis?</p>
+                            </div>
+                        </article>
+                      </section>`;
+    let section_three = '<section class="contact"><h3>Внимание</h3><p>Оставьте ваши контактные данные <br/>и наш консультант свяжется с вами</p><form id="application" action="application.php" method="POST" name="application "><input name="name" id="applicationName" maxlength="20" placeholder="Введите ваше имя" required /><input name="email" type="email" id="applicationEmail" maxlength="30" placeholder="Введите ваш E-mail" required/><input name="telephone" type="Tel" id="applicationTelephone" maxlength="20" placeholder="Введите ваш телефон" required /><button class="applicationButton" type="submit" form="application"> Оставить заявку </button></form></section>';
     
     $( '#link_main_one' ).click(function() {
         $('section').detach();
